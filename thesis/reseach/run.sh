b84e15ead747fc16bd17a7111d3d7c56849d5586
daoducmanh194 <daomanhkaiyako@gmail.com>
2023-06-30 13:57:56 +0700

    19-1 rrr

--- thesis/reseach/run.sh
+++ thesis/reseach/run.sh
@@ -1,6 +1,6 @@
 DATA_ROOT=/kaggle/input/pascalvoc12/pascalvoc12_new/
 DATASET=voc
-TASK=15-1
+TASK=19-1
 EPOCH=10
 BATCH=16
 LOSS=bce_loss
@@ -8,7 +8,7 @@ LR=0.01
 THRESH=0.7
 MEMORY=100
 
-python main.py --data_root ${DATA_ROOT} --model deeplabv3_resnet101_attention --gpu_id 0 --crop_val --lr ${LR} \
+python main.py --data_root ${DATA_ROOT} --model deeplabv3_resnet101 --gpu_id 0 --crop_val --lr ${LR} \
     --batch_size ${BATCH} --train_epoch ${EPOCH}  --loss_type ${LOSS} \
     --dataset ${DATASET} --task ${TASK} --overlap --lr_policy poly \
     --pseudo --pseudo_thresh ${THRESH} --freeze  --bn_freeze  \
