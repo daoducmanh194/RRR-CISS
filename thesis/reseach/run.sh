375e6a1f36cc4549afc26d32eb15418783450c67
daoducmanh194 <daomanhkaiyako@gmail.com>
2023-07-09 15:45:02 +0700

    voc 15-1 rrr mwm=0 no freeze

--- thesis/reseach/run.sh
+++ thesis/reseach/run.sh
@@ -11,6 +11,6 @@ MEMORY=0
 python main.py --data_root ${DATA_ROOT} --model deeplabv3_resnet101 --gpu_id 0 --crop_val --lr ${LR} \
     --batch_size ${BATCH} --train_epoch ${EPOCH}  --loss_type ${LOSS} \
     --dataset ${DATASET} --task ${TASK} --overlap --lr_policy poly \
-    --pseudo --pseudo_thresh ${THRESH} --freeze  --bn_freeze  \
-    --unknown --amp --mem_size ${MEMORY}
+    --pseudo --pseudo_thresh ${THRESH}  --bn_freeze  \
+    --unknown --w_transfer --amp --mem_size ${MEMORY}
     
