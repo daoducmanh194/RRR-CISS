adafeb656d2641f83b9564ee9a626a74b7e5db57
daoducmanh194 <daomanhkaiyako@gmail.com>
2023-07-16 15:14:10 +0700

    thresh = 0.0

--- thesis/reseach/run.sh
+++ thesis/reseach/run.sh
@@ -5,12 +5,12 @@ EPOCH=10
 BATCH=16
 LOSS=bce_loss
 LR=0.01
-THRESH=0.7
+THRESH=0.0
 MEMORY=0
 
 python main.py --data_root ${DATA_ROOT} --model deeplabv3_resnet101 --gpu_id 0 --crop_val --lr ${LR} \
     --batch_size ${BATCH} --train_epoch ${EPOCH}  --loss_type ${LOSS} \
     --dataset ${DATASET} --task ${TASK} --overlap --lr_policy poly \
-    --pseudo --pseudo_thresh ${THRESH}  --bn_freeze  \
+    --pseudo --pseudo_thresh ${THRESH} --freeze  --bn_freeze  \
     --unknown --w_transfer --amp --mem_size ${MEMORY}
     
